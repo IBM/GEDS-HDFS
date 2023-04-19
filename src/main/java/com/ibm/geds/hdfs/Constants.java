ca6b181d0a7c5b3b34cdee0047cbe5644d9d11ca
Pascal Spörri <psp@zurich.ibm.com>
2023-04-19 10:04:14 +0200

    Integrate support to configure io_thread_pool_size, available_local_memory and available_local_storage.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

--- src/main/java/com/ibm/geds/hdfs/Constants.java
+++ src/main/java/com/ibm/geds/hdfs/Constants.java
@@ -14,4 +14,7 @@ public class Constants {
     public static final String LOCAL_STORAGE_PATH = "local_storage_path";
     public static final String CACHE_BLOCK_SIZE = "cache_block_size";
     public static final String HTTP_SERVER_PORT = "http_server_port";
+    public static final String AVAILABLE_LOCAL_STORAGE = "available_local_storage";
+    public static final String AVAILABLE_LOCAL_MEMORY = "available_local_memory";
+    public static final String IO_THREAD_POOL_SIZE = "io_thread_pool_size";
 }
