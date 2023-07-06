dadecd5995e95d922a5f0ba324ef54f40613161d
Pascal Spörri <psp@zurich.ibm.com>
2023-07-06 17:13:45 +0200

    Enable builds on Github CI.
    Signed-off-by: Pascal Spörri <psp@zurich.ibm.com>

---
+++ .github/prepare-build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023- IBM Inc. All rights reserved
+# SPDX-License-Identifier: Apache2.0
+#
+set -euo pipefail
+
+ROOT="$(cd "`dirname $0`/../" && pwd)"
+cd "${ROOT}"
+
+VERSION=$(git describe --tags || echo "vrev-$(git rev-parse --short HEAD)")
+VERSION="${VERSION:1}"
+echo "Version: ${VERSION}"
+
+# Change revision.
+FILES=(
+  "${ROOT}/build.sbt"
+)
+
+for i in "${FILES[@]}"; do
+  echo "Replacing SNAPSHOT in ${i} with ${VERSION}"
+  sed -i "s/SNAPSHOT/${VERSION}/g" "${i}"
+done
+
+SCALA_VERSION=${SCALA_VERSION:-""}
+if [[ "${SCALA_VERSION:0:4}" == "2.13" ]]; then
+  echo "Removing tests from build since ${SCALA_VERSION} is not supported!"
+  sed -i "/TRAVIS_SCALA_WORKAROUND_REMOVE_LINE/d" "${ROOT}/build.sbt"
+  rm -rf "${ROOT}/src/test"
+fi
+
+
+GEDS_VERSION=${GEDS_VERSION:-"0.8.4"}
+wget -nv "https://github.com/IBM/GEDS/releases/download/v${GEDS_VERSION}/geds-x86_64-debian11.5-${GEDS_VERSION}-Release.tar.gz"
+tar xf "geds-x86_64-debian11.5-${GEDS_VERSION}-Release.tar.gz"
